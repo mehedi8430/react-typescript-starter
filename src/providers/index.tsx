115dfb6a18133a469bb951969363fe2d1e97d3f4
MEHEDI HASAN <mehedialhasan1999@gmail.com>
2025-12-06 23:01:49 +0600

    feat(redux): integrate Redux Toolkit with authentication and user management with rtk query setup

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,8 +1,12 @@
 import { RouterProvider } from "react-router";
-import { ThemeProvider } from "../theme";
+import { ThemeProvider } from "./ThemeProvider";
 import router from "../routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "../redux/store";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,13 +24,17 @@ const queryClient = new QueryClient({
 
 export default function Providers() {
   return (
-    <ThemeProvider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        {import.meta.env.NODE_ENV === "development" && (
-          <ReactQueryDevtools initialIsOpen={false} />
-        )}
-      </QueryClientProvider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider>
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+            {import.meta.env.NODE_ENV === "development" && (
+              <ReactQueryDevtools initialIsOpen={false} />
+            )}
+          </QueryClientProvider>
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
   );
 }
